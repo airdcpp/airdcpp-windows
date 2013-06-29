3a244908b3897f90c2ec482ed6ffadea612c4839
maksis <maksis@ec3e54a6-43b8-4a3e-9c49-2a733a73d9fe>
2013-06-29 17:28:39 +0000

    shutdown fix
    git-svn-id: http://svn.airdcpp.net/airsvn/trunk@1589 ec3e54a6-43b8-4a3e-9c49-2a733a73d9fe

--- windows/main.cpp
+++ windows/main.cpp
@@ -433,6 +433,7 @@ static int Run(LPTSTR /*lpstrCmdLine*/ = NULL, int nCmdShow = SW_SHOWDEFAULT)
 	nRet = theLoop.Run();
 	_Module.RemoveMessageLoop();
 
+	WinUtil::splash.reset(nullptr);
 	WinUtil::runPendingUpdate();
 	return nRet;
 }
