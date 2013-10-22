e57f3ac51107391599da99dc7344b41a54f014e1
night <->
2013-10-22 22:48:09 +0300

    - versioninfo script changes

--- gitversioninfo.bat
+++ gitversioninfo.bat
@@ -1,10 +1,28 @@
 @echo off
-call git describe --abbrev=4 > version.txt
-for /F "tokens=1,2,3 delims=-" %%a in (version.txt) do ( 
-echo #define GIT_TAG %%a > %1client\version-revno.inc
-echo #define GIT_COMMIT %%b >> %1client\version-revno.inc
-echo #define GIT_HASH "%%c" >> %1client\version-revno.inc
-)
-for /F "tokens=*" %%a in ('git rev-list HEAD --count') do echo #define GIT_COMMIT_COUNT %%a >> %1client\version-revno.inc
+set file=%1client\version-revno.inc
+set tmpfile=%1client\version-revno.inc.tmp
 
+call git describe --abbrev=4 > version.txt
+
+for /F "tokens=1,2,3 delims=-" %%a in (version.txt) do ( 
+	echo #define GIT_TAG %%a > %tmpfile%
+	echo #define GIT_COMMIT %%b >> %tmpfile%
+	echo #define GIT_HASH "%%c" >> %tmpfile%
+)
+
+for /F "tokens=*" %%a in ('git rev-list HEAD --count') do echo #define GIT_COMMIT_COUNT %%a >> %tmpfile%
+
+fc /b %tmpfile% %file% > nul
+if errorlevel 1 goto :versionChanged
+
+echo No Version changes detected, using the old version file
+goto :end
+	
+:versionChanged
+	echo Version changes detected updating the version file
+	copy /y %tmpfile% %file% > nul
+goto :end
+	
+:end
+ECHO Y | DEL %tmpfile%
 ECHO Y | DEL version.txt
