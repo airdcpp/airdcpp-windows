e20bb85644046bc2b0a733c4b8e381e422d66753
maksis <maksis@adrenaline-network.com>
2014-01-01 21:19:00 +0200

    Update the copyright year

--- windows/TempShare_dlg.cpp
+++ windows/TempShare_dlg.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2012-2013 AirDC++ team
+* Copyright (C) 2012-2014 AirDC++ team
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
