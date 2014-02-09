76c1b78b5681da13482a76d0314a9a8c9dbd326f
maksis <maksis@adrenaline-network.com>
2014-02-09 21:36:59 +0200

    - Automatically check for updates when the update channel was changed - Add .gitignore

--- make_release.bat
+++ make_release.bat
@@ -4,7 +4,7 @@ set solutionDir=%CD%
 
 set fileName=airdcpp_VERSION_x86.7z
 cd installer
-7za a -t7z %solutionDir%\%fileName% EmoPacks Themes dcppboot.xml popup.bmp
+7za a -t7z %solutionDir%\%fileName% EmoPacks Themes dcppboot.xml
 cd ..
 cd compiled\Win32
 7za a -t7z %solutionDir%\%fileName% AirDC.exe AirDC.pdb
@@ -13,12 +13,8 @@ cd %solutionDir%
 
 set fileName=airdcpp_VERSION_x64.7z
 cd installer
-7za a -t7z %solutionDir%\%fileName% EmoPacks Themes dcppboot.xml popup.bmp
+7za a -t7z %solutionDir%\%fileName% EmoPacks Themes dcppboot.xml
 cd ..
 cd compiled\x64
 7za a -t7z %solutionDir%\%fileName% AirDC.exe AirDC.pdb
-cd %solutionDir%
-
-set fileName=airdcpp_VERSION_src.7z
-7za a -t7z %solutionDir%\%fileName% boost bzip2 client geoip installer leveldb linux MakeDefs miniupnpc minizip natpmp openssl regex res snappy windows zlib AirDC.rc AirDC.sln Changelog_AirDC.txt client.vcxproj client.vcxproj.filters EN_Example.xml SConstruct StrongDC.vcxproj StrongDC.vcxproj.filters
 cd %solutionDir%
