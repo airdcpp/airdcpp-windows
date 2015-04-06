17da6d4ba2651f28df84356fb52142437d5cb4b5
night <night.fi@gmail.com>
2015-04-06 08:37:42 +0300

    update Copyright years.

--- windows/OMenu.h
+++ windows/OMenu.h
@@ -1,7 +1,7 @@
 /* 
  * Copyright (C) 2003-2004 "Opera", <opera at home dot se>
  *
- * Copyright (C) 2011-2014 AirDC++ Team
+ * Copyright (C) 2011-2015 AirDC++ Team
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
