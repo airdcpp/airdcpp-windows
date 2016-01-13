2ce81133896cf4b4a68b8b944b52a7468bd2baf8
night-z <night.fi@gmail.com>
2016-01-13 19:44:19 +0200

    Update copyright years

--- windows/AutosearchSearchTimesPage.h
+++ windows/AutosearchSearchTimesPage.h
@@ -1,7 +1,7 @@
 #pragma once
 
 /*
-* Copyright (C) 2011-2015 AirDC++ Project
+* Copyright (C) 2011-2016 AirDC++ Project
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
