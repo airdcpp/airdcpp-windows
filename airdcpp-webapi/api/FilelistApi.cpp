0fab27c7a88f46f39d75ce45f133616d2e3d694e
maksis <maksis@adrenaline-network.com>
2016-01-25 12:39:34 +0200

    API: no share profile for remote filelists

--- airdcpp-webapi/api/FilelistApi.cpp
+++ airdcpp-webapi/api/FilelistApi.cpp
@@ -156,7 +156,7 @@ namespace webserver {
 			{ "total_files", totalFiles },
 			{ "total_size", totalSize },
 			{ "read", aList->isRead() },
-			{ "share_profile", Serializer::serializeShareProfileSimple(aList->getShareProfile()) },
+			{ "share_profile", aList->getIsOwnList() ? Serializer::serializeShareProfileSimple(aList->getShareProfile()) : nullptr },
 		};
 	}
 
