2054fa58a093ee62e89e94c40330fefd20df8604
maksis <maksis@adrenaline-network.com>
2016-03-10 16:28:32 +0200

    QueueApi: update the "time finished" prop on QueueManagerListener::BundleStatusChanged

--- airdcpp-webapi/api/QueueApi.cpp
+++ airdcpp-webapi/api/QueueApi.cpp
@@ -436,7 +436,7 @@ namespace webserver {
 	}
 
 	void QueueApi::on(QueueManagerListener::BundleStatusChanged, const BundlePtr& aBundle) noexcept {
-		onBundleUpdated(aBundle, { PROP_STATUS }, "bundle_status");
+		onBundleUpdated(aBundle, { PROP_STATUS, PROP_TIME_FINISHED }, "bundle_status");
 	}
 
 	void QueueApi::on(QueueManagerListener::BundleSources, const BundlePtr& aBundle) noexcept {
