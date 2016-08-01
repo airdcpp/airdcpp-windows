292b5941dd6ad6e12e5e9fa661232a6f20c3ef57
maksis <maksis@adrenaline-network.com>
2016-08-01 10:04:55 +0300

    Use case-insensitive comparison for directory list base

--- airdcpp/airdcpp/DirectoryListing.cpp
+++ airdcpp/airdcpp/DirectoryListing.cpp
@@ -384,7 +384,7 @@ void ListLoader::startTag(const string& name, StringPairList& attribs, bool simp
 
 			// Validate the parsed base path
 			{
-				if (b != base) {
+				if (Util::stricmp(b, base) != 0) {
 					throw AbortException("The base directory specified in the file list (" + b + ") doesn't match with the expected base (" + base + ")");
 				}
 
