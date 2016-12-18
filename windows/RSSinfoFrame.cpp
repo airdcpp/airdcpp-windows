6b6d16de2ca64661e9fab9dfac6ec1ce49d7a4cb
night-z <night.fi@gmail.com>
2016-12-18 21:18:18 +0200

    Add auto open at startup option for Auto search

--- windows/RSSinfoFrame.cpp
+++ windows/RSSinfoFrame.cpp
@@ -216,7 +216,7 @@ LRESULT RssInfoFrame::onContextMenu(UINT /*uMsg*/, WPARAM wParam, LPARAM lParam,
 			bool allRelease = all_of(items, [=](const ItemInfo* a) { return a->isRelease; });
 			if (allRelease) {
 				//auto search menus
-				appendDownloadMenu(menu, DownloadBaseHandler::TYPE_SECONDARY, true, boost::none, title, false);
+				appendDownloadMenu(menu, DownloadBaseHandler::TYPE_SECONDARY, true, boost::none, boost::none, false);
 				menu.appendSeparator();
 			}
 
