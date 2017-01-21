1404d2d71139bd42c2099db745662366f98632e5
maksis <maksis@adrenaline-network.com>
2017-01-21 16:09:11 +0200

    Header cleanup

--- windows/QueueFrame.h
+++ windows/QueueFrame.h
@@ -27,6 +27,7 @@
 
 #include <airdcpp/DirectoryListingManager.h>
 #include <airdcpp/QueueManagerListener.h>
+#include <airdcpp/QueueItem.h>
 #include <airdcpp/TaskQueue.h>
 
 #define STATUS_MSG_MAP 19
