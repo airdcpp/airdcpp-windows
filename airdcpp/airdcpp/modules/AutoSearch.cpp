23ae57709f47483b0828ee36472aee28365f030c
night-z <night.fi@gmail.com>
2017-01-23 15:44:47 +0200

    Fix crash on failed auto search items

--- airdcpp/airdcpp/modules/AutoSearch.cpp
+++ airdcpp/airdcpp/modules/AutoSearch.cpp
@@ -288,10 +288,13 @@ void AutoSearch::updateStatus() noexcept {
 		}
 	} else {
 		auto maxBundle = *boost::max_element(bundles, Bundle::StatusOrder());
-		if (ActionHookError::matches(maxBundle->getHookError(), SHARE_SCANNER_HOOK_ID, SHARE_SCANNER_ERROR_MISSING)) {
-			status = AutoSearch::STATUS_FAILED_MISSING;
-		} else if (ActionHookError::matches(maxBundle->getHookError(), SHARE_SCANNER_HOOK_ID, SHARE_SCANNER_ERROR_INVALID_CONTENT)) {
-			status = AutoSearch::STATUS_FAILED_EXTRAS;
+		if(maxBundle->getStatus() == Bundle::STATUS_VALIDATION_ERROR) {
+			if (ActionHookError::matches(maxBundle->getHookError(), SHARE_SCANNER_HOOK_ID, SHARE_SCANNER_ERROR_MISSING)) {
+				status = AutoSearch::STATUS_FAILED_MISSING;
+			}
+			else if (ActionHookError::matches(maxBundle->getHookError(), SHARE_SCANNER_HOOK_ID, SHARE_SCANNER_ERROR_INVALID_CONTENT)) {
+				status = AutoSearch::STATUS_FAILED_EXTRAS;
+			}
 		} else {
 			status = AutoSearch::STATUS_QUEUED_OK;
 		}
