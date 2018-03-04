722c0cf9d66a3cc4f96b2ec9812d700af02ca029
maksis <maksis@adrenaline-network.com>
2018-03-04 12:57:58 +0200

    Add scripts for pulling changes from subtrees

---
+++ scripts/update-api.sh
@@ -0,0 +1,2 @@
+git fetch airdcpp-webapi
+git subtree pull --prefix airdcpp-webapi airdcpp-webapi master --squash
---
+++ scripts/update-core.sh
@@ -0,0 +1,2 @@
+git fetch airdcpp
+git subtree pull --prefix airdcpp airdcpp master --squash
