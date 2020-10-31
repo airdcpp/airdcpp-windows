c8e3dc40fdd9836fb2ad23d6400ede2814a49d54
maksis <maksis@adrenaline-network.com>
2020-10-31 18:28:57 +0200

    Update copyright years

--- windows/OMenu.cpp
+++ windows/OMenu.cpp
@@ -1,7 +1,7 @@
 /* 
  * Copyright (C) 2003-2004 "Opera", <opera at home dot se>
  *
- * Copyright (C) 2011-2019 AirDC++ Team
+ * Copyright (C) 2011-2021 AirDC++ Team
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
