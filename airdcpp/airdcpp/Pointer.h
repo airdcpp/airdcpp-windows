eb2f6580226e778257d17c884eb418e275320f5b
maksis <maksis@adrenaline-network.com>
2024-09-05 17:41:26 +0300

    Remove clang warning spam

--- airdcpp/airdcpp/Pointer.h
+++ airdcpp/airdcpp/Pointer.h
@@ -50,7 +50,7 @@ protected:
 private:
 	friend void intrusive_ptr_add_ref(intrusive_ptr_base* p) { ++p->ref; }
 
-#if defined (__GNUC__)
+#if defined (__GNUC__) && !defined(__llvm__) && !defined(__INTEL_COMPILER)
 // warning: ‘long unsigned int __atomic_sub_fetch_8(volatile void*, long unsigned int, int)’ writing 8 bytes into a region of size 0 overflows the destination
 // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=107694
 #pragma GCC diagnostic push
